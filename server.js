bb4d8e859a11e8681c95598b9669697afdaffd61
Mehmet Acar <mehmetacar683@gmail.com>
2019-11-16 21:08:40 +0300

    mqtt server created

---
+++ server.js
@@ -0,0 +1,27 @@
+var mosca = require('mosca');
+
+var settings = {
+    port: 1883, // mqtt default port number
+};
+
+var server = new mosca.Server(settings);
+
+// when a cliend connected the mqtt server, run
+server.on('clientConnected', function(client) {
+    console.log('a client connected', client.id);
+});
+
+// fired when a message is received
+server.on('published', function(packet, client) {
+    console.log('Published', packet.payload);
+});
+
+server.on('ready', setup);
+
+// fired when the mqtt server is ready
+function setup() {
+    console.log('Mosca server is up and running');
+}
+
+// etc.
+// topic -> chat rooms
